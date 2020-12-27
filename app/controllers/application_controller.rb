86e89c43aaf1b7201bf763ac0ee3c713d7cc491a
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-27 19:42:44 +0000

    add JWT methodes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::API
+     def user_serializer(user)
+        {
+          name: user.name,
+          email: user.email,
+          id: user.id
+        }
+      end
+    
+      def current_user
+        begin
+          @current_user ||= User.find(decode_token_and_get_user_id)
+        rescue
+          return nil
+        end
+      end
+    
+      def logged_in?
+        !!current_user
+      end
+    
+      def generate_token(payload)
+        JWT.encode(payload, ENV['JWT_TOKEN_SECRET'])
+      end
+    
+      def decode_token_and_get_user_id
+        JWT.decode(request.headers["Authorization"], ENV['JWT_TOKEN_SECRET'])[0]["id"]
+      end
+
+    # JWT is match that you are person who given us information more that will protect the information
 end
