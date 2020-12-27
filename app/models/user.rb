c0a96e6b3d1ffa59440fc9ece1a3508658fccc7a
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-27 00:25:57 +0000

    add has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has has_secure_password
+    has_secure_password
     has_many :secrets
     validates :email, :name, presence: true
     validates :email, uniqueness: true
